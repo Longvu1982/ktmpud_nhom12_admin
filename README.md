e8122f259b327bc80ca17729c2fcbfa790c2fecf
Longvu1982 <tranngochiep200047@gmail.com>
2023-02-12 23:31:03 +0700

    Fix

--- README.md
+++ README.md
@@ -1,70 +1,31 @@
-# Getting Started with Create React App
+# Group 12 admin page
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+## How to set up
 
-In the project directory, you can run:
+Install `node` and `npm` to your computer
+Recommend version: `node 16.14.2` 
 
-### `npm start`
+### First
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Clone the project 
+`git clone https://github.com/Longvu1982/ktmpud_nhom12_admin.git`
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+### Install dependencies
+Run `npm i`
 
-### `npm test`
+### Create env variable
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Create `.env` file in the root folder
 
-### `npm run build`
+`REACT_APP_FIREBASE_KEY = "AIzaSyAfTPWUhiG_WpKk4tlwqN5ryTLCSJFwQ3A"
+REACT_APP_FIREBASE_APP_ID = "1:65607578846:web:82681d11efdb46c659be4f"`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+*** Note that this ID is for researching purpose, do not use this for personal use
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+### Start the project
 
-### `npm run eject`
+Run `npm start`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,95 +9,117 @@ import { db } from "../firebase";
 import "./Login.css";
 import { login } from "../features/userSlice";
 import { doc, getDoc } from "firebase/firestore";
+import { AltRoute } from "@mui/icons-material";
 
 const Login = () => {
-	const [isLoading, setLoading] = useState(false);
-	// input binding
-	const [adminName, setAdminName] = useState("");
-	const [password, setPassword] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  // input binding
+  const [adminName, setAdminName] = useState("");
+  const [password, setPassword] = useState("");
 
-	// firebase auth
-	const auth = getAuth();
+  // firebase auth
+  const auth = getAuth();
 
-	// redux dispatch
-	const dispatch = useDispatch();
+  // redux dispatch
+  const dispatch = useDispatch();
 
-	const handleLogin = async (e) => {
-		e.preventDefault();
-		setLoading(true);
-		const userDocRef = doc(db, "users", adminName);
-		const docSnap = await getDoc(userDocRef);
-		if (docSnap?.data()?.role === 2) {
-			signInWithEmailAndPassword(auth, adminName, password)
-				.then((user) => dispatch(login({ email: user.user.email, uid: user.user.uid })))
-				.catch((e) => {
-					if (e.code === "auth/wrong-password") {
-						setLoading(false);
-						alert("Sai tài khoản / mật khẩu");
-						return;
-					}
-				});
-		} else {
-			setLoading(false);
-			alert("Sai tài khoản / mật khẩu");
-			return;
-		}
-	};
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (adminName === "" || password === "") {
+      alert("Hãy điền đầy đủ thông tin");
+      return;
+    }
+    setLoading(true);
+    const userDocRef = doc(db, "users", adminName);
+    const docSnap = await getDoc(userDocRef);
+    if (docSnap?.data()?.role === 2) {
+      signInWithEmailAndPassword(auth, adminName, password)
+        .then((user) =>
+          dispatch(login({ email: user.user.email, uid: user.user.uid }))
+        )
+        .catch((e) => {
+          if (e.code === "auth/wrong-password") {
+            setLoading(false);
+            alert("Sai tài khoản / mật khẩu");
+            return;
+          }
+        });
+    } else {
+      setLoading(false);
+      alert("Sai tài khoản / mật khẩu");
+      return;
+    }
+  };
 
-	return (
-		<div className="login-container">
-			<div className="screen">
-				<div className="screen__content">
-					<form className="login">
-						<p style={{ marginBottom: 30, fontSize: 28, fontWeight: "bold", color: "#389ca0" }}>Nhóm 12 - Admin</p>
-						<div className="login__field">
-							<div>
-								<FontAwesomeIcon icon={faUser} color="#4accd1" />
-							</div>
-							<input
-								value={adminName}
-								onChange={(e) => {
-									setAdminName(e.target.value);
-								}}
-								type="text"
-								className="login__input"
-								placeholder="Tài khoản"
-							></input>
-						</div>
-						<div className="login__field">
-							<div>
-								<FontAwesomeIcon icon={faChevronCircleRight} color="#4accd1" />
-							</div>
-							<input
-								value={password}
-								onChange={(e) => {
-									setPassword(e.target.value);
-								}}
-								type="password"
-								className="login__input"
-								placeholder="Mật khẩu"
-							></input>
-						</div>
-						<button
-							className="button login__submit flex items-center justify-start"
-							onClick={(e) => {
-								handleLogin(e);
-							}}
-						>
-							<span className="button__text">Đăng nhập NGAY</span>
-							{isLoading && <FontAwesomeIcon size="lg" icon={faSpinner} className="ml-10 animate-spin" />}
-						</button>
-					</form>
-				</div>
-				<div className="screen__background">
-					<span className="screen__background__shape screen__background__shape4"></span>
-					<span className="screen__background__shape screen__background__shape3"></span>
-					<span className="screen__background__shape screen__background__shape2"></span>
-					<span className="screen__background__shape screen__background__shape1"></span>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="login-container">
+      <div className="screen">
+        <div className="screen__content">
+          <form className="login">
+            <p
+              style={{
+                marginBottom: 30,
+                fontSize: 28,
+                fontWeight: "bold",
+                color: "#389ca0",
+              }}
+            >
+              Nhóm 12 - Admin
+            </p>
+            <div className="login__field">
+              <div>
+                <FontAwesomeIcon icon={faUser} color="#4accd1" />
+              </div>
+              <input
+                value={adminName}
+                onChange={(e) => {
+                  setAdminName(e.target.value);
+                }}
+                type="text"
+                className="login__input"
+                placeholder="Tài khoản"
+              ></input>
+            </div>
+            <div className="login__field">
+              <div>
+                <FontAwesomeIcon icon={faChevronCircleRight} color="#4accd1" />
+              </div>
+              <input
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                type="password"
+                className="login__input"
+                placeholder="Mật khẩu"
+              ></input>
+            </div>
+            <button
+              className="button login__submit flex items-center justify-start"
+              onClick={(e) => {
+                handleLogin(e);
+              }}
+            >
+              <span className="button__text">Đăng nhập NGAY</span>
+              {isLoading && (
+                <FontAwesomeIcon
+                  size="lg"
+                  icon={faSpinner}
+                  className="ml-10 animate-spin"
+                />
+              )}
+            </button>
+          </form>
+        </div>
+        <div className="screen__background">
+          <span className="screen__background__shape screen__background__shape4"></span>
+          <span className="screen__background__shape screen__background__shape3"></span>
+          <span className="screen__background__shape screen__background__shape2"></span>
+          <span className="screen__background__shape screen__background__shape1"></span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
